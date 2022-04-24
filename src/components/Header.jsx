a724d9cc593ddb4fac2f0f0e64c55aae679c27a3
Maks <83704092+m1x-developer@users.noreply.github.com>
2022-04-24 20:46:04 +0300

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://bestprogrammer.ru/wp-content/uploads/2020/04/React-Native.jpg" alt=""/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>ava + descr</div>
+            <div>my posts
+                <div>post</div>
+                <div>post</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
