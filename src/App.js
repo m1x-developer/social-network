7c715832abe63b68403317d0efcb37f4759850bd
Maks <83704092+m1x-developer@users.noreply.github.com>
2022-04-26 18:42:08 +0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import Profile from "./components/Profile";
+import {Routes, Route} from "react-router-dom";
+import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+
 
 const App = () => {
-	return (
-		<div className='app-wrapper'>
-			<Header />
-			<Navbar />
-            <Profile/>
-		</div>
-	)
+    return (
+        <>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className="content">
+                    <Routes>
+                        <Route path="/" exact element={<Profile/>}/>
+                        <Route path="/messages" exact element={<Dialogs/>}/>
+                        <Route path="/news" exact element={'news'}/>
+                    </Routes>
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default App
