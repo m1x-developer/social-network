e41127018fe873f73fd9d675eeb49eab5430b78c
Maks <83704092+m1x-developer@users.noreply.github.com>
2022-04-26 22:03:26 +0300

    adding logic

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    Link
+} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <ul>
+                <Link className="link" to="/">Profile</Link>
+                <Link className="link" to="/messages">Messages</Link>
+                <Link className="link" to="/news">News</Link>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
